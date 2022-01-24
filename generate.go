4d55350a53fa4a28c725cdc169e7ea66ab2821de
Andrew Arrow <oneone@gmail.com>
2022-01-24 08:22:28 -0800

    adding site example

--- generate.go
+++ generate.go
@@ -1,3 +1,5 @@
-package main
+package vugusite
 
-//go:generate vugugen -s
+//go:generate vugugen -s -r -skip-go-mod -skip-main ./app/components
+//go:generate vugugen -s -r ./app/pages
+//go:generate vgrgen -r ./app/pages
